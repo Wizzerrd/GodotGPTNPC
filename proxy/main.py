fae8be6ae68514f7b4523d0717e95c816b877a58
Wizzerrd <luisthelaffitte@gmail.com>
2024-05-14 16:03:03 -0700

    add characters folder, route to get character by name

--- proxy/main.py
+++ proxy/main.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask
+import os, json
+from flask import Flask, request
 from openai import OpenAI
 
 app = Flask(__name__)
@@ -9,4 +9,9 @@ client.api_key = os.environ["OPENAI_API_KEY"]
 @app.route("/")
 
 def hello_world():
-    return "<p>hello world</p>"
+    return "<p>hello world</p>" 
+    
+@app.route("/characters/<character_name>", methods=['GET'])
+def get_character(character_name):
+    print(character_name)
+    return (character_name,200)
