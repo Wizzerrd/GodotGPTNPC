f44adc05e4e0ed01f5b8c4e3de08e7f18acf02d3
Wizzerrd <luisthelaffitte@gmail.com>
2024-05-24 16:17:34 -0700

    rename pirate to knight, update demo

--- godot/main.gd
+++ godot/main.gd
@@ -9,7 +9,7 @@ func _ready():
 	$GodotGPTNPCHandler.connect_to_host("127.0.0.1", "", 5000, true, false)
 	
 func post_message(message: String):
-	var url = "http://127.0.0.1/characters/pirate/messages"
+	var url = "http://127.0.0.1/characters/knight/messages"
 	var headers = ["Content-Type: application/json", "Accept: text/event-stream"]
 	var method = HTTPClient.METHOD_POST
 	var body = JSON.stringify({"message":message})
